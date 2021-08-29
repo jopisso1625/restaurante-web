5f8bea442cc062c89bb5a06f366521072f0280d2
sigfried1625 <joseleal87@gmail.com>
2021-08-28 19:07:43 -0500

    Aplicando BEM

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,13 @@
-let imagenes = document.querySelectorAll('.gallery_img')
+let imagenes = document.querySelectorAll('.gallery__img')
 let modal = document.querySelector('#modal');
-let img = document.querySelector('#modal_img');
-let boton = document.querySelector('#modal_boton');
+let img = document.querySelector('#modal__img');
+let boton = document.querySelector('#modal__boton');
 
 for (let i = 0; i < imagenes.length; i++) {
     imagenes[i].addEventListener('click', function (e) {
-        modal.classList.toggle("modal_open");
+        modal.classList.toggle("modal__open");
         img.setAttribute("src", e.target.src)
     })
 }
 
-boton.addEventListener('click', function () { modal.classList.toggle("modal_open") })
+boton.addEventListener('click', function () { modal.classList.toggle("modal__open") })
